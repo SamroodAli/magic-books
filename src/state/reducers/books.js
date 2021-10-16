677bfcd3a47fc92a24a25fee64cc03b454972395
samroodAli <devsamrood@gmail.com>
2021-10-16 11:18:06 +0530

    Reducer:Create placeholder reducer for books

---
+++ src/state/reducers/books.js
@@ -0,0 +1,10 @@
+// import Actions
+// import action types
+
+const booksReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default booksReducer;
