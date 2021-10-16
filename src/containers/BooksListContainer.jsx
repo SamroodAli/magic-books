ec353ae8b4b54bdb29c9d27af641beaad1fd816b
samroodAli <devsamrood@gmail.com>
2021-10-16 16:05:18 +0530

    RemoveBook:Give removeBook function from bookListContainer to BooksList

--- src/containers/BooksListContainer.jsx
+++ src/containers/BooksListContainer.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux';
 import BooksList from '../components/BooksList';
+import useActions from '../hooks/useActions';
 
 const BooksListContainer = () => {
   const books = useSelector(({ books }) => books);
-  return <BooksList books={books} />;
+  const { removeBook } = useActions();
+  return <BooksList books={books} handleRemoveBook={removeBook} />;
 };
 
 export default BooksListContainer;
