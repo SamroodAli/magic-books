80e230d38f86bd50411fca7391581491f1bf0a5c
samroodAli <devsamrood@gmail.com>
2021-10-16 13:04:49 +0530

    Form:BookForm in presentational components using react final form

---
+++ src/components/BooksFormContainer.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Form, Field } from 'react-final-form';
+import PropTypes from 'prop-types';
+
+const CATEGORIES = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
+const BookForm = ({ initialValues, onFormSubmit }) => {
+  const renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    const {
+ name, checked, value, onChange, onFocus, onBlur 
+} = input;
+    return (
+      <div className={className}>
+        <label htmlFor={label}>{label}</label>
+        <input
+          id={label}
+          name={name}
+          checked={checked}
+          value={value}
+          onChange={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          autoComplete="off"
+        />
+        {renderError(meta)}
+      </div>
+    );
+  };
+
+  const onSubmit = (formValues) => {
+    onFormSubmit(formValues);
+  };
+
+  const renderForm = ({ handleSubmit }) => (
+    <form onSubmit={handleSubmit} className="ui form error">
+      <Field name="title" component={renderInput} label="Enter Title" />
+
+      <Field name="favoriteColor" component="select">
+        {CATEGORIES.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </Field>
+      <button type="submit" className="ui button primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+      errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.category) {
+      errors.description = 'You must choose a category';
+    }
+
+    return errors;
+  };
+
+  return (
+    <Form
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validate={validate}
+      render={renderForm}
+    />
+  );
+};
+
+BookForm.defaultProps = {
+  initialValues: { title: '', category: '' },
+};
+BookForm.propTypes = {
+  initialValues: PropTypes.shape({
+    title: PropTypes.string,
+    category: PropTypes.string,
+  }),
+  onFormSubmit: PropTypes.func.isRequired,
+};
+
+export default BookForm;
--- src/containers/BooksFormContainer.jsx
+++
@@ -1,7 +0,0 @@
-const BooksForm = () => (
-  <div>
-    <p>Book Form</p>
-  </div>
-);
-
-export default BooksForm;
