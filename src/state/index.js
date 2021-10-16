cf1876f842b04120efbe11580458dd4f2bc996d7
samroodAli <devsamrood@gmail.com>
2021-10-16 12:16:58 +0530

    Books:Give redux store initial State

--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,37 @@
 import { createStore } from 'redux';
+import { nanoid } from 'nanoid';
 import rootReducer from './reducers';
 
 export * as actionCreators from './action-creators';
-export default createStore(rootReducer);
+
+const initialState = {
+  books: [
+    {
+      id: nanoid(),
+      title: 'Ego is the Enemy',
+      category: 'Stoicism',
+    },
+    {
+      id: nanoid(),
+      title: 'Life of Pi',
+      category: 'Adventure',
+    },
+    {
+      id: nanoid(),
+      title: 'To Kill a MockingBird',
+      category: 'Classic',
+    },
+    {
+      id: nanoid(),
+      title: 'Watchmen',
+      category: 'Comics',
+    },
+    {
+      id: nanoid(),
+      title: 'The Hunger Games Trilogy',
+      category: 'Sci-fi',
+    },
+  ],
+};
+
+export default createStore(rootReducer, initialState);
