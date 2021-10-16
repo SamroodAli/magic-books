636778c8ea62dc164dfcd1db6e51808c51576cc2
samroodAli <devsamrood@gmail.com>
2021-10-16 11:22:44 +0530

    Component:Create placeholder table for BooksList component

---
+++ src/containers/BooksList.jsx
@@ -0,0 +1,16 @@
+const BooksList = () => (
+  <table>
+    <thead>
+      <tr>
+        <th colSpan="2">The table header</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>The table body</td>
+        <td>with two columns</td>
+      </tr>
+    </tbody>
+  </table>
+);
+export default BooksList;
