f5125cfdc44878aeeb07f5c0778b6ad0cb12f158
samroodAli <devsamrood@gmail.com>
2021-10-17 21:27:12 +0530

    Style: Style book card update progress section

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -9,12 +9,12 @@ const Book = ({
     <div className="book-card">
       <div className="book-details">
         <p className="School-of">{category}</p>
-        <h5>{title}</h5>
+        <h5 className="Book-Title">{title}</h5>
         <p className="author">{`${faker.name.firstName()} ${faker.name.lastName()}`}</p>
         <div className="book-buttons">
           <button type="button" className="Comments">Comments</button>
           <button type="button" className="Remove" onClick={removeBook}>Remove</button>
-          <button type="button" className="Edit" onClick={removeBook}>Edit</button>
+          <button type="button" className="Edit">Edit</button>
         </div>
       </div>
       <div className="book-progress">
@@ -35,9 +35,11 @@ const Book = ({
         </div>
       </div>
       <div className="book-chapter">
-        <p>Current chapter  </p>
-        <p>Chapter 17</p>
-        <button type="button" className="update-book-btn">Update progress</button>
+        <p className="Current-Chapter">Current chapter  </p>
+        <p className="Current-Lesson">Chapter 17</p>
+        <button type="button" className="update-book-btn btn">
+          <p className="Update-progress">Update Progress</p>
+        </button>
       </div>
     </div>
 
